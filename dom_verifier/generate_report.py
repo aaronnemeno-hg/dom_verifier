5d6bce0506d66ed142f7854412683089e916bde0
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2022-05-26 13:42:09 +0800

    Added new key for each dict item in HYLINK_SITE_VERIFY constant. This key is called 'require_pixel' where if set to True, pixel script tags are required for the site.

--- dom_verifier/generate_report.py
+++ dom_verifier/generate_report.py
@@ -18,211 +18,277 @@ SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
 
 HYLINK_SITE_VERIFY = {
     # LOCAL
-    # 'http://uclacn.local':{
-        # 'require_ga': False,
-        # 'require_gtm': False
-    # },
-    # 'http://magogallery.local':{
-        # 'require_ga': False,
-        # 'require_gtm': False
-    # },
+    'http://uclacn.local':{
+        'require_ga': False,
+        'require_gtm': False,
+        'require_pixel': False,
+    },
+    'http://magogallery.local':{
+        'require_ga': False,
+        'require_gtm': False,
+        'require_pixel': False
+    },
     ## PRODUCTION
-    'https://hylinkdore.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://hylinktravel.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://hylinkventures.com/':{
-        'require_ga': True,
-        'require_gtm': False
-    },
-    'https://hylinkquantum.com/':{
-        'require_ga': True,
-        'require_gtm': False
-    },
-    'https://hylinkhelix.com/':{
-        'require_ga': True,
-        'require_gtm': False
-    },
-    'https://magogalleryglobal.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://oneleggedpigeon.com/':{
-        'require_ga': True,
-        'require_gtm': False
-    },
-    'https://hylinkpublicrelations.com/':{
-        'require_ga': True,
-        'require_gtm': False
-    },
-    'https://mena.uclahealth.org/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://culturelle.co.kr/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://culturelle.jp/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://sg.nanboya.global/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://hk.nanboya.global/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://culturelle.sg/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://cn.uclahealth.org/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://shyn.com/?fts=0&preview_theme_id=120649089124':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-dore.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-travel.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-ventures.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-hylinkquantum.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-helix.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-mago.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://staging-olp.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-publicrelations.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://newstaging-uclahealth.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://staging-culturellekr-wp.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://staging-culturellejp-wp.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://staging-culturellesg-wp.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://naboya-sg-staging.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://naboya-hk-staging.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://uclacn-stg.hylinkgroup.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://dmp-staging.hylinkgroup.com/home':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://esteelaudercampaignpage.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://esteelaudercelebrate.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://campaign.travelbeautifully.cn/staging/edmp_390/power/cdf_replenishment/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://wasabi.elcapps.com/appstartup_sanya/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/appstartup_haikou':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/apphomepageslideshow_sanya':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/apphomepageslideshow_haikou':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://wasabi.elcapps.com/appflighthomepage_sanya/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'https://wasabi.elcapps.com/appflighthomepage_haikou/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/apphomepagefeed_sanya':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/appmembercenterpage_sanya':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/apphomepagefeed_haikou':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/appvacationhomepage_sanya':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://wasabi.elcapps.com/appvacationhomepage_haikou':{
-        'require_ga': False,
-        'require_gtm': False
-    },
-    'http://replenishment.elcapps.com/':{
-        'require_ga': False,
-        'require_gtm': False
-    },
+    # 'https://hylinkdore.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://hylinktravel.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://hylinkventures.com/':{
+        # 'require_ga': True,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://hylinkquantum.com/':{
+        # 'require_ga': True,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://hylinkhelix.com/':{
+        # 'require_ga': True,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://magogalleryglobal.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://oneleggedpigeon.com/':{
+        # 'require_ga': True,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://hylinkpublicrelations.com/':{
+        # 'require_ga': True,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://mena.uclahealth.org/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://culturelle.co.kr/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://culturelle.jp/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://sg.nanboya.global/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://hk.nanboya.global/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://culturelle.sg/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://cn.uclahealth.org/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://shyn.com/?fts=0&preview_theme_id=120649089124':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-dore.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-travel.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-ventures.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-hylinkquantum.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-helix.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-mago.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://staging-olp.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-publicrelations.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://newstaging-uclahealth.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://staging-culturellekr-wp.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://staging-culturellejp-wp.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://staging-culturellesg-wp.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://naboya-sg-staging.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://naboya-hk-staging.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://uclacn-stg.hylinkgroup.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://dmp-staging.hylinkgroup.com/home':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://esteelaudercampaignpage.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://esteelaudercelebrate.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://campaign.travelbeautifully.cn/staging/edmp_390/power/cdf_replenishment/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://wasabi.elcapps.com/appstartup_sanya/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/appstartup_haikou':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/apphomepageslideshow_sanya':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/apphomepageslideshow_haikou':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://wasabi.elcapps.com/appflighthomepage_sanya/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'https://wasabi.elcapps.com/appflighthomepage_haikou/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/apphomepagefeed_sanya':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/appmembercenterpage_sanya':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/apphomepagefeed_haikou':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/appvacationhomepage_sanya':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://wasabi.elcapps.com/appvacationhomepage_haikou':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://replenishment.elcapps.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://jmllnycampaign.elcapps.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://elsupremepost.elcapps.com/index.html':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
+    # 'http://tfsatinmattereplenishment.elcapps.com/':{
+        # 'require_ga': False,
+        # 'require_gtm': False,
+        # 'require_pixel': False,
+    # },
 }
 
 items = []
@@ -301,8 +367,10 @@ if __name__ == "__main__":
             # temporarily going to leave gtm aside as it's not required
             # as per this sheet: https://docs.google.com/spreadsheets/d/1vds3C2Sl-QfMKsqqm2zrPU1ohL_CqAcZROhsRFgCp-c/edit#gid=0
             gtm_required = HYLINK_SITE_VERIFY[item['url']]['require_gtm']
+
+            pixel_required = HYLINK_SITE_VERIFY[item['url']]['require_pixel']
         except KeyError:
-            ga_required, gtm_required = False, False
+            ga_required, gtm_required, pixel_required = False, False, False
 
         # generate block item section text
         bi_section_text = "\n∙ *url:* {}"\
@@ -310,12 +378,20 @@ if __name__ == "__main__":
         bi_section_text += "\n    ◦ response status code: *{}*"\
                 .format(item['status_code'])
 
-        has_src, _ = script_tag_has(item['gt_src'])
+        has_gt_src, _ = script_tag_has(item['gt_src'])
         #bi_section_text += "\n    ◦ has script tag with src: *{}*"\
         #        .format(_)
 
-        has_code, _ = script_tag_has(item['gt_code'])
-        bi_section_text += "\n    ◦ has script tag with code: *{}*"\
+        has_gt_code, _ = script_tag_has(item['gt_code'])
+        bi_section_text += "\n    ◦ has google analytics script tag with code: *{}*"\
+                .format(_)
+
+        has_pixel_src, _ = script_tag_has(item['pixel_src'])
+        bi_section_text += "\n    ◦ has pixel script tag with src: *{}*"\
+                .format(_)
+
+        has_pixel_code, _ = script_tag_has(item['pixel_code'])
+        bi_section_text += "\n    ◦ has pixel script tag with code: *{}*"\
                 .format(_)
 
         block_item = {"type": "section", "text": {"type": "mrkdwn"}}
@@ -324,8 +400,10 @@ if __name__ == "__main__":
         # update block item
         block_item['text'].update({'text': bi_section_text})
 
-        if ga_required and not has_code:
+        if (ga_required and not has_gt_code) or \
+            (pixel_required and (not has_pixel_src and not has_pixel_code)):
             # google analytics is required and has no script with code
+            # pixel is required
             blocks.append(block_item)
 
     # if blocks length == 2, this means all sites have passed the
